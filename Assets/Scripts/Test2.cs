edc97333060edcd0da544594aca0b24b84d287f0
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-05-08 10:53:43 +0600

    new script added

---
+++ Assets/Scripts/Test2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+    
+    public Transform objectToPlace;
+    public Camera gameCamera;
+
+    void Update() {
+        
+        Ray ray = gameCamera.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo)) {
+            objectToPlace.position = hitInfo.point;
+            objectToPlace.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+}
