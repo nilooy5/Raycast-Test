02cfc77088d7268830a27d023ec95e44157c40a1
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-05-08 09:51:34 +0600

    new scene added and destroyer using raycasting added

--- Assets/Scripts/Test1.cs
+++ Assets/Scripts/Test1.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test1 : MonoBehaviour {
-    // Start is called before the first frame update
+    public LayerMask mask;
     void Start() {
         
     }
-
-    // Update is called once per frame
     void Update() {
-        
+
+        Ray ray = new Ray (transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 100, mask, QueryTriggerInteraction.Ignore)) {
+            print(hitInfo.collider.gameObject.name);
+            Destroy (hitInfo.collider.gameObject);
+            Debug.DrawLine (ray.origin, hitInfo.point, Color.red);
+        } else {
+            Debug.DrawLine (ray.origin, ray.origin + ray.direction * 100, Color.green);
+        }
     }
 }
